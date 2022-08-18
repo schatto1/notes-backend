7d9c28f6e8eabcb92fbc02c88ee468fe48868615
Sumon Chattopadhyay <sumon.chattopadhyay@utah.edu>
2022-08-17 19:54:30 -0700

    refactor: updated codebase with example code for part4.1

--- mongo.js
+++ mongo.js
@@ -1,13 +1,16 @@
 const mongoose = require('mongoose')
 
-if (process.argv.length < 3) {
-  console.log('Please provide the password as an argument: node mongo.js <password>')
+if (process.argv.length<3) {
+  console.log('give password as argument')
   process.exit(1)
 }
 
-const password = encodeURIComponent(process.argv[2])
+const password = process.argv[2]
 
-const url = `mongodb+srv://fullstack:${password}@cluster0.pmkf0.mongodb.net/noteApp?retryWrites=true&w=majority`
+const url =
+  `mongodb+srv://fullstack:${password}@cluster0.o1opl.mongodb.net/noteApp?retryWrites=true&w=majority`
+
+mongoose.connect(url)
 
 const noteSchema = new mongoose.Schema({
   content: String,
@@ -17,21 +20,23 @@ const noteSchema = new mongoose.Schema({
 
 const Note = mongoose.model('Note', noteSchema)
 
-mongoose
-  .connect(url)
-  .then((result) => {
-    console.log('connected')
+const note = new Note({
+  content: 'CSS is hard',
+  date: new Date(),
+  important: false,
+})
 
-    const note = new Note({
-      content: 'HTML is Easy',
-      date: new Date(),
-      important: true,
-    })
-
-    return note.save()
-  })
-  .then(() => {
+// eslint-disable-next-line no-constant-condition
+if ( false ) {
+  note.save().then(() => {
     console.log('note saved!')
-    return mongoose.connection.close()
+    mongoose.connection.close()
   })
-  .catch((err) => console.log(err))
+}
+
+Note.find({}).then(result => {
+  result.forEach(note => {
+    console.log(note)
+  })
+  mongoose.connection.close()
+})
