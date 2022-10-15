f2c8d26bfc89916ffc6eee1c2e73b5922e2ede02
Sumon Chattopadhyay <sumon.chattopadhyay@utah.edu>
2022-10-15 10:44:59 -0700

    fix: adds error handling to POST

--- controllers/notes.js
+++ controllers/notes.js
@@ -26,9 +26,12 @@ notesRouter.post('/', async (request, response, next) => {
     important: body.important || false,
     date: new Date(),
   })
-
-  const savedNote = await note.save()
-  response.status(201).json(savedNote)
+  try {
+    const savedNote = await note.save()
+    response.status(201).json(savedNote)
+  } catch(exception) {
+    next(exception)
+  }
 })
 
 notesRouter.delete('/:id', (request, response, next) => {
