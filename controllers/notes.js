d612d106c67baef6580c607fc0d7d80cd82583e2
Sumon Chattopadhyay <sumon.chattopadhyay@utah.edu>
2022-10-17 19:08:39 -0700

    refactor: syncs code with end of 4-5

--- controllers/notes.js
+++ controllers/notes.js
@@ -6,6 +6,15 @@ notesRouter.get('/', async (request, response) => {
   response.json(notes)
 })
 
+notesRouter.get('/:id', async (request, response) => {
+  const note = await Note.findById(request.params.id)
+  if (note) {
+    response.json(note.toJSON())
+  } else {
+    response.status(404).end()
+  }
+})
+
 notesRouter.post('/', async (request, response) => {
   const body = request.body
 
@@ -17,15 +26,6 @@ notesRouter.post('/', async (request, response) => {
 
   const savedNote = await note.save()
   response.status(201).json(savedNote)
-})
-
-notesRouter.get('/:id', async (request, response) => {
-  const note = await Note.findById(request.params.id)
-  if (note) {
-    response.json(note)
-  } else {
-    response.status(404).end()
-  }
 })
 
 notesRouter.delete('/:id', async (request, response) => {
--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -1,19 +1,16 @@
-const supertest = require('supertest')
 const mongoose = require('mongoose')
-const helper = require('./test_helper')
+const supertest = require('supertest')
 const app = require('../app')
+
 const api = supertest(app)
 
 const Note = require('../models/note')
+const helper = require('./test_helper')
+
 
 beforeEach(async () => {
   await Note.deleteMany({})
-
-  let noteObject = new Note(helper.initialNotes[0])
-  await noteObject.save()
-
-  noteObject = new Note(helper.initialNotes[1])
-  await noteObject.save()
+  await Note.insertMany(helper.initialNotes)
 })
 
 test('notes are returned as json', async () => {
